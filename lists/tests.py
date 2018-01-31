25df7037ce4dfcabc53926cd215fb1f0abc42f02
djangotutor <djangotutor@gmail.com>
2018-01-31 17:42:25 +0900

    home.html 템플릿과 home_page() 뷰의 POST 요청 처리 연계
    단일 사용자의 첫번째 입력 FT 성공

--- lists/tests.py
+++ lists/tests.py
@@ -5,3 +5,12 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post(
+                '/',
+                data={'item_text': '새 아이템'}
+        )
+
+        self.assertIn('새 아이템', response.content.decode('utf8'))
+        self.assertTemplateUsed(response, 'home.html')
