6ea74d43d5ac1e1b8ff3e81f2b06ea7f15d466e1
djangotutor <djangotutor@gmail.com>
2018-01-31 18:23:28 +0900

    아이템(작업) 모델 Item 작성

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -14,3 +16,22 @@ class HomePageTest(TestCase):
 
         self.assertIn('새 아이템', response.content.decode('utf8'))
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫번째 아이템'
+        first_item.save()
+        second_item = Item()
+        second_item.text = '두번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫번째 아이템')
+        self.assertEqual(second_saved_item.text, '두번째 아이템')
+
